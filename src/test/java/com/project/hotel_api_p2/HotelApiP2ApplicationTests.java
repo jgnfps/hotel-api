89ed8891dc95e0d4dbdc3a047e38de53a6ca54b4
jgnfps <cupreguiado@gmail.com>
2025-10-23 10:09:20 -0300

    primeiro commit

---
+++ src/test/java/com/project/hotel_api_p2/HotelApiP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hotel_api_p2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiP2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
