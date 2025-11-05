4dcc11847475ee2120fe7fe0cb0fa680e12f3376
jgnfps <cupreguiado@gmail.com>
2025-11-04 23:42:19 -0300

    feat: create quartoDTOs

---
+++ src/main/java/com/project/hotel_api_p2/dto/QuartoRequestDTO.java
@@ -0,0 +1,26 @@
+package com.project.hotel_api_p2.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record QuartoRequestDTO(
+
+        @NotBlank(message = "O número do quarto é obrigatório")
+        String numero,
+
+        @NotBlank(message = "O tipo do quarto é obrigatório")
+        String tipo,
+
+        @NotNull(message = "O preço da diária é obrigatório")
+        @Positive(message = "O preço da diária deve ser maior que zero")
+        BigDecimal precoDiaria,
+
+        @NotNull(message = "A disponibilidade deve ser informada")
+        Boolean disponivel,
+
+        @NotNull(message = "O ID do hotel é obrigatório")
+        Long hotelId
+) { }
---
+++ src/main/java/com/project/hotel_api_p2/dto/QuartoResponseDTO.java
@@ -0,0 +1,13 @@
+package com.project.hotel_api_p2.dto;
+
+import java.math.BigDecimal;
+
+public record QuartoResponseDTO (
+        Long id,
+        String numero,
+        String tipo,
+        BigDecimal precoDiaria,
+        Boolean disponivel,
+        String hotel
+){
+}
